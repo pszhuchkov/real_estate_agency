071389e4b38dec2bfc7ae9c5055f13cdded3e281
pzhuchkov <pzhuchkov@gmail.com>
2020-12-30 17:47:51 +0300

    Add field for likes

--- property/admin.py
+++ property/admin.py
@@ -11,10 +11,10 @@ class FlatAdmin(admin.ModelAdmin):
     )
     list_editable = ('new_building',)
     list_filter = ('new_building', 'rooms_number', 'has_balcony')
+    raw_id_fields = ('liked_by',)
 
 
 @admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('complainter', 'flat',)
     list_display = ('complainter', 'flat', 'text',)
-
--- property/models.py
+++ property/models.py
@@ -24,6 +24,10 @@ class Flat(models.Model):
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
     new_building = models.NullBooleanField("Новостройка")
 
+    liked_by = models.ManyToManyField(
+        User, verbose_name='Кто лайкнул', related_name='liked_posts'
+    )
+
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
 
