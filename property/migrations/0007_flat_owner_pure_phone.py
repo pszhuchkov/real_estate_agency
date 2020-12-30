7c5c3797fcb399165a7ad4d9c9925f9407cada12
pzhuchkov <pzhuchkov@gmail.com>
2020-12-31 00:50:36 +0300

    Fix migration

--- property/migrations/0007_flat_owner_pure_phone.py
+++ property/migrations/0007_flat_owner_pure_phone.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='flat',
             name='owner_pure_phone',
-            field=phonenumber_field.modelfields.PhoneNumberField(blank=True, max_length=128, region=None),
+            field=phonenumber_field.modelfields.PhoneNumberField(blank=True, max_length=128, region=None, verbose_name='Нормализованный номер владельца'),
         ),
     ]
