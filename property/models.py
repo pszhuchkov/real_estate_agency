294305935e273ec1eaab591e4e61cfab595c2557
pzhuchkov <pzhuchkov@gmail.com>
2021-01-11 12:52:04 +0300

    Add index to fields of Owner model

--- property/models.py
+++ property/models.py
@@ -52,10 +52,12 @@ class Complaint(models.Model):
 
 
 class Owner(models.Model):
-    owner = models.CharField("ФИО владельца", max_length=200)
-    owners_phonenumber = models.CharField("Номер владельца", max_length=20)
+    owner = models.CharField("ФИО владельца", max_length=200, db_index=True)
+    owners_phonenumber = models.CharField(
+        "Номер владельца", max_length=20, db_index=True
+    )
     owner_pure_phone = PhoneNumberField(
-        "Нормализованный номер владельца", blank=True
+        "Нормализованный номер владельца", blank=True, db_index=True
     )
     flats = models.ManyToManyField(
         'Flat', verbose_name='Квартиры в собственности',
