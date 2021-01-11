3d16a4c62b578060881709dceee9285677d965d9
pzhuchkov <pzhuchkov@gmail.com>
2021-01-11 12:57:28 +0300

    Relate flats and owners

--- property/models.py
+++ property/models.py
@@ -5,7 +5,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class Flat(models.Model):
-    owner = models.CharField("ФИО владельца", max_length=200)
+    owner_deprecated = models.CharField("ФИО владельца", max_length=200)
     owners_phonenumber = models.CharField("Номер владельца", max_length=20)
     owner_pure_phone = PhoneNumberField(
         "Нормализованный номер владельца", blank=True
