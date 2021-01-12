9fc9947f07a462732f4de56ac2fbc3d666e9fb0a
pzhuchkov <pzhuchkov@gmail.com>
2021-01-12 19:52:01 +0300

    Refactor data migration

--- property/migrations/0004_auto_20201230_0041.py
+++ property/migrations/0004_auto_20201230_0041.py
@@ -6,10 +6,7 @@ from django.db import migrations
 def set_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
     for flat in Flat.objects.all():
-        if flat.construction_year >= 2015:
-            flat.new_building = True
-        else:
-            flat.new_building = False
+        flat.new_building = flat.construction_year >= 2015
         flat.save()
 
 
